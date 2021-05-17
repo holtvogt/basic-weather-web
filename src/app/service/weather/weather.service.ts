13c459b40bc0b349387ef0c3ca78fe6a89da3c38
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2021-05-18 00:39:03 +0200

    Add weather service

--- src/app/service/weather/weather.service.ts
+++ src/app/service/weather/weather.service.ts
@@ -1,3 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { OpenWeatherMapEndpoint } from './openWeatherMap.endpoint';
+
+@Injectable()
 export class WeatherService {
-    
+
+    private forecastTodayURL: string;
+    private forecastThisWeekURL: string;
+    private forecastNextWeekURL: string;
+
+    constructor(private http: HttpClient) {
+        this.forecastTodayURL = OpenWeatherMapEndpoint.TODAY;
+        this.forecastThisWeekURL = OpenWeatherMapEndpoint.THIS_WEEK;
+        this.forecastNextWeekURL = OpenWeatherMapEndpoint.NEXT_WEEK;
+    }
+
+    getForecastTodayByCoordinates(latitude: number, longitude: number) {
+        let url = this.forecastTodayURL + '&lat=' + latitude + '&lon=' + longitude;
+        this.http.get(url).toPromise().then(data => console.log(data));
+    }
+
+    getForecastThisWeekByCoordinates(latitude: number, longitude: number) {
+        let url = this.forecastThisWeekURL + '&lat=' + latitude + '&lon=' + longitude;
+        this.http.get(url).toPromise().then(data => console.log(data));
+    }
+
+    getForecastNextWeekByCoordinates(latitude: number, longitude: number) {
+        let url = this.forecastNextWeekURL + '&lat=' + latitude + '&lon=' + longitude;
+        this.http.get(url).toPromise().then(data => console.log(data));
+    }
 }
