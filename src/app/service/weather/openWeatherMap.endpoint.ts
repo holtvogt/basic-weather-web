e677fe46766f6855bfc6f87d03fdf0b3458eac38
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2021-05-19 01:00:38 +0200

    Add json parameter to OpenWeatherMap endpoints

--- src/app/service/weather/openWeatherMap.endpoint.ts
+++ src/app/service/weather/openWeatherMap.endpoint.ts
@@ -1,7 +1,7 @@
 import { OpenWeatherMap } from "./openWeatherMap";
 
 export const OpenWeatherMapEndpoint = {
-    TODAY: 'https://pro.openweathermap.org/data/2.5/forecast/climate?appid=' + OpenWeatherMap.API_KEY + '&cnt=1&units=metric',
-    THIS_WEEK: 'https://pro.openweathermap.org/data/2.5/forecast/climate?appid=' + OpenWeatherMap.API_KEY + '&cnt=7&units=metric',
-    NEXT_WEEK: 'https://pro.openweathermap.org/data/2.5/forecast/climate?appid=' + OpenWeatherMap.API_KEY + '&cnt=14&units=metric'
+    TODAY: 'https://pro.openweathermap.org/data/2.5/forecast/climate?mode=json&cnt=1&units=metric&appid=' + OpenWeatherMap.API_KEY,
+    THIS_WEEK: 'https://pro.openweathermap.org/data/2.5/forecast/climate?mode=json&cnt=7&units=metric&appid=' + OpenWeatherMap.API_KEY,
+    NEXT_WEEK: 'https://pro.openweathermap.org/data/2.5/forecast/climate?mode=json&cnt=14&units=metric&appid=' + OpenWeatherMap.API_KEY
 };
