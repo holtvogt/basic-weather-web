85e42f92a4648f44681bd98c52f0a635ef1d1fd2
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2021-05-22 00:13:20 +0200

    Add geolocation functionality

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -19,77 +19,84 @@ export class CardComponent implements OnInit {
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
-    switch (this.forecast) {
-      /**
-       * Today.
-       */
-      case Forecast.TODAY:
-        this.weatherService.getForecastTodayByCoordinates(49.006889, 8.403653).then(weather => this.weather = weather);
-        break;
-
-      /**
-       * This week.
-       */
-      case Forecast.THIS_WEEK:
-        switch (this.weekday) {
-          case Weekday.FIRST:
-            this.weatherService.getForecastThisWeekByCoordinates(49.006889, 8.403653).then(weather => this.weather = weather[0]);
+    // Get current position
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+        switch (this.forecast) {
+          /**
+           * Today.
+           */
+          case Forecast.TODAY:
+            this.weatherService.getForecastTodayByCoordinates(latitude, longitude).then(weather => this.weather = weather);
             break;
-          case Weekday.SECOND:
-            this.weatherService.getForecastThisWeekByCoordinates(49.006889, 8.403653).then(weather => this.weather = weather[1]);
+    
+          /**
+           * This week.
+           */
+          case Forecast.THIS_WEEK:
+            switch (this.weekday) {
+              case Weekday.FIRST:
+                this.weatherService.getForecastThisWeekByCoordinates(latitude, longitude).then(weather => this.weather = weather[0]);
+                break;
+              case Weekday.SECOND:
+                this.weatherService.getForecastThisWeekByCoordinates(latitude, longitude).then(weather => this.weather = weather[1]);
+                break;
+              case Weekday.THIRD:
+                this.weatherService.getForecastThisWeekByCoordinates(latitude, longitude).then(weather => this.weather = weather[2]);
+                break;
+              case Weekday.FOURTH:
+                this.weatherService.getForecastThisWeekByCoordinates(latitude, longitude).then(weather => this.weather = weather[3]);
+                break;
+              case Weekday.FIFTH:
+                this.weatherService.getForecastThisWeekByCoordinates(latitude, longitude).then(weather => this.weather = weather[4]);
+                break;
+              case Weekday.SIXTH:
+                this.weatherService.getForecastThisWeekByCoordinates(latitude, longitude).then(weather => this.weather = weather[5]);
+                break;
+              case Weekday.SEVENTH:
+                this.weatherService.getForecastThisWeekByCoordinates(latitude, longitude).then(weather => this.weather = weather[6]);
+                break; 
+              default:
+                break;
+            }
             break;
-          case Weekday.THIRD:
-            this.weatherService.getForecastThisWeekByCoordinates(49.006889, 8.403653).then(weather => this.weather = weather[2]);
+    
+          /**
+           * Next week.
+           */
+          case Forecast.NEXT_WEEK:
+            switch (this.weekday) {
+              case Weekday.FIRST:
+                this.weatherService.getForecastNextWeekByCoordinates(latitude, longitude).then(weather => this.weather = weather[0]);
+                break;
+              case Weekday.SECOND:
+                this.weatherService.getForecastNextWeekByCoordinates(latitude, longitude).then(weather => this.weather = weather[1]);
+                break;
+              case Weekday.THIRD:
+                this.weatherService.getForecastNextWeekByCoordinates(latitude, longitude).then(weather => this.weather = weather[2]);
+                break;
+              case Weekday.FOURTH:
+                this.weatherService.getForecastNextWeekByCoordinates(latitude, longitude).then(weather => this.weather = weather[3]);
+                break;
+              case Weekday.FIFTH:
+                this.weatherService.getForecastNextWeekByCoordinates(latitude, longitude).then(weather => this.weather = weather[4]);
+                break;
+              case Weekday.SIXTH:
+                this.weatherService.getForecastNextWeekByCoordinates(latitude, longitude).then(weather => this.weather = weather[5]);
+                break;
+              case Weekday.SEVENTH:
+                this.weatherService.getForecastNextWeekByCoordinates(latitude, longitude).then(weather => this.weather = weather[6]);
+                break; 
+              default:
+                break;
+            }
             break;
-          case Weekday.FOURTH:
-            this.weatherService.getForecastThisWeekByCoordinates(49.006889, 8.403653).then(weather => this.weather = weather[3]);
-            break;
-          case Weekday.FIFTH:
-            this.weatherService.getForecastThisWeekByCoordinates(49.006889, 8.403653).then(weather => this.weather = weather[4]);
-            break;
-          case Weekday.SIXTH:
-            this.weatherService.getForecastThisWeekByCoordinates(49.006889, 8.403653).then(weather => this.weather = weather[5]);
-            break;
-          case Weekday.SEVENTH:
-            this.weatherService.getForecastThisWeekByCoordinates(49.006889, 8.403653).then(weather => this.weather = weather[6]);
-            break; 
           default:
             break;
         }
-        break;
-
-      /**
-       * Next week.
-       */
-      case Forecast.NEXT_WEEK:
-        switch (this.weekday) {
-          case Weekday.FIRST:
-            this.weatherService.getForecastNextWeekByCoordinates(49.006889, 8.403653).then(weather => this.weather = weather[0]);
-            break;
-          case Weekday.SECOND:
-            this.weatherService.getForecastNextWeekByCoordinates(49.006889, 8.403653).then(weather => this.weather = weather[1]);
-            break;
-          case Weekday.THIRD:
-            this.weatherService.getForecastNextWeekByCoordinates(49.006889, 8.403653).then(weather => this.weather = weather[2]);
-            break;
-          case Weekday.FOURTH:
-            this.weatherService.getForecastNextWeekByCoordinates(49.006889, 8.403653).then(weather => this.weather = weather[3]);
-            break;
-          case Weekday.FIFTH:
-            this.weatherService.getForecastNextWeekByCoordinates(49.006889, 8.403653).then(weather => this.weather = weather[4]);
-            break;
-          case Weekday.SIXTH:
-            this.weatherService.getForecastNextWeekByCoordinates(49.006889, 8.403653).then(weather => this.weather = weather[5]);
-            break;
-          case Weekday.SEVENTH:
-            this.weatherService.getForecastNextWeekByCoordinates(49.006889, 8.403653).then(weather => this.weather = weather[6]);
-            break; 
-          default:
-            break;
-        }
-        break;
-      default:
-        break;
+      });
     }
   }
 }
