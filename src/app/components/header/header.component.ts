1f2680484837e95fbdd2d3916f944a2968ca9f3c
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2021-06-01 01:12:22 +0200

    Display location in header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,6 +5,7 @@ import {
   ViewChild
 } from '@angular/core';
 import { Router } from '@angular/router';
+import { GeocodingService } from 'src/app/service/geocoding/geocoding.service';
 
 @Component({
   selector: 'app-header',
@@ -18,9 +19,46 @@ export class HeaderComponent implements OnInit {
   @ViewChild('navMenu')
   navMenu!: ElementRef;
 
-  constructor(private router: Router) { }
+  location!: string;
+
+  constructor(private router: Router, private geocodingService: GeocodingService) {}
+
+  private showUserLocation() {
+    // Get current position
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => this.setLocation(position), error => this.showError(error));
+      // Geolocation is not supported
+    } else {
+      document.getElementById('location')!.innerHTML = 'Geolocation is not supported by this browser.';
+    }
+  }
+
+  private setLocation(position: GeolocationPosition) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    this.geocodingService.getLocationByCoordinates(latitude, longitude).then(location => this.location = location);
+  }
+
+  private showError(error: GeolocationPositionError) {
+    let locationHeadline = document.getElementById('location');
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        locationHeadline!.innerHTML = "<strong><em>User denied request for Geolocation.</em></strong>"
+        break;
+      case error.POSITION_UNAVAILABLE:
+        locationHeadline!.innerHTML = "<strong><em>Location information is unavailable.</em></strong>"
+        break;
+      case error.TIMEOUT:
+        locationHeadline!.innerHTML = "<strong><em>The request to get user location timed out.</em></strong>"
+        break;
+      default:
+        locationHeadline!.innerHTML = "<strong><em>An unknown error occurred while accessing user location.</em></strong>"
+        break;
+    }
+  }
 
   ngOnInit(): void {
+    this.showUserLocation();
   }
 
   navigateHome() {
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,8 +3,6 @@ import {
   OnInit
 } from '@angular/core';
 
-import { GeocodingService } from '../../service/geocoding/geocoding.service';
-
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -12,45 +10,7 @@ import { GeocodingService } from '../../service/geocoding/geocoding.service';
 })
 export class MainComponent implements OnInit {
 
-  location!: string;
+  constructor() {}
 
-  constructor(private geocodingService: GeocodingService) {}
-
-  private showUserLocation() {
-    // Get current position
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => this.setLocation(position), error => this.showError(error));
-      // Geolocation is not supported
-    } else {
-      document.getElementById('content-location')!.innerHTML = 'Geolocation is not supported by this browser.';
-    }
-  }
-
-  private setLocation(position: GeolocationPosition) {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    this.geocodingService.getLocationByCoordinates(latitude, longitude).then(location => this.location = location);
-  }
-
-  private showError(error: GeolocationPositionError) {
-    let locationHeadline = document.getElementById('content-location');
-    switch(error.code) {
-      case error.PERMISSION_DENIED:
-        locationHeadline!.innerHTML = "<strong><em>User denied the request for Geolocation.</em></strong>"
-        break;
-      case error.POSITION_UNAVAILABLE:
-        locationHeadline!.innerHTML = "<strong><em>Location information is unavailable.</em></strong>"
-        break;
-      case error.TIMEOUT:
-        locationHeadline!.innerHTML = "<strong><em>The request to get user location timed out.</em></strong>"
-        break;
-      default:
-        locationHeadline!.innerHTML = "<strong><em>An unknown error occurred while accessing user location.</em></strong>"
-        break;
-    }
-  }
-
-  ngOnInit(): void {
-    this.showUserLocation();
-  }
+  ngOnInit(): void {}
 }
