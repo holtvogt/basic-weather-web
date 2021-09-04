30dabe6296af5f12ad1a8eedfebb90ed8893c4a5
Bjoern Holtvogt <bjoern.holtvogt@gmail.com>
2021-09-04 10:29:54 +0200

    Apply format changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { MainComponent } from './components/main/main.component';
-import { NextWeekComponent } from './components/next-week/next-week.component';
-import { ThisWeekComponent } from './components/this-week/this-week.component';
-import { TodayComponent } from './components/today/today.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { MainComponent } from "./components/main/main.component";
+import { NextWeekComponent } from "./components/next-week/next-week.component";
+import { ThisWeekComponent } from "./components/this-week/this-week.component";
+import { TodayComponent } from "./components/today/today.component";
 
+/**
+ * The route navigations.
+ */
 const routes: Routes = [
-  { path: '', redirectTo: 'home', component: MainComponent },
-  { path: 'home', component: MainComponent },
-  { path: 'today', component: TodayComponent },
-  { path: 'thisWeek', component: ThisWeekComponent },
-  { path: 'nextWeek', component: NextWeekComponent }
+	{ path: "", redirectTo: "home", component: MainComponent },
+	{ path: "home", component: MainComponent },
+	{ path: "today", component: TodayComponent },
+	{ path: "thisWeek", component: ThisWeekComponent },
+	{ path: "nextWeek", component: NextWeekComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainComponent } from './components/main/main.component';
-import { CardComponent } from './components/card/card.component';
-import { HeaderComponent } from './components/header/header.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MainComponent } from "./components/main/main.component";
+import { CardComponent } from "./components/card/card.component";
+import { HeaderComponent } from "./components/header/header.component";
 
-import { GeocodingService } from './service/geocoding/geocoding.service';
-import { WeatherService } from './service/weather/weather.service';
-import { TodayComponent } from './components/today/today.component';
-import { ThisWeekComponent } from './components/this-week/this-week.component';
-import { NextWeekComponent } from './components/next-week/next-week.component';
+import { GeocodingService } from "./service/geocoding/geocoding.service";
+import { WeatherService } from "./service/weather/weather.service";
+import { TodayComponent } from "./components/today/today.component";
+import { ThisWeekComponent } from "./components/this-week/this-week.component";
+import { NextWeekComponent } from "./components/next-week/next-week.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainComponent,
-    CardComponent,
-    HeaderComponent,
-    TodayComponent,
-    ThisWeekComponent,
-    NextWeekComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule
-  ],
-  providers: [
-    GeocodingService,
-    WeatherService
-  ],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		MainComponent,
+		CardComponent,
+		HeaderComponent,
+		TodayComponent,
+		ThisWeekComponent,
+		NextWeekComponent,
+	],
+	imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+	providers: [GeocodingService, WeatherService],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
