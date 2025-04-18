e2c6b8f69f6c7ded35fc7053647ab019cb627cf4
Bjoern Holtvogt <bjoern.holtvogt@gmail.com>
2025-04-18 18:48:41 +0200

    Improve structure and organization of header and main content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.css"],
-    standalone: false
+    standalone: true,
+    imports: [
+        HeaderComponent, 
+        RouterModule
+    ]
 })
 export class AppComponent {
 	title = "Basic Weather";
